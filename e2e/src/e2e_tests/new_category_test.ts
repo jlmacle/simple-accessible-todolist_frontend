5b4861ce5532ca557491b6413bf44965976ecfcc
Jean - Louis Macle <jeanlouis.macle@gmail.com>
2020-12-02 18:15:37 -0600

    Work on the e2e test where a category is added.

--- e2e/src/e2e_tests/new_category_test.ts
+++ e2e/src/e2e_tests/new_category_test.ts
@@ -1,33 +1,96 @@
 import { browser, by, WebElement } from "protractor";
 
-describe('Testing the adding of a new category', 
+describe('Testing the adding of a new category and item.', 
             function(){
-                it('A test of the suite', 
+                it('Testing the adding of a new category.', 
                     function() {
                         browser.driver.get("http://localhost:4200");
                         browser.driver.findElement(by.id("new_category_input_field")).sendKeys("Protractor test category");
                         browser.driver.findElement(by.id("add_category_button")).click();
-                        browser.driver.sleep(5000);
-                        // Checking if the corresponding HTML element can be find in the DOM
-                        // TODO: find the id associated to the category "Protractor test category",
-                        // in order to build the id used to find the HTML elements.
                         
-                        let all_li_elements_Promise = browser.driver.findElements(by.tagName("li"));
-                        let all_li_elements = [];
-                        let protractor_test_category_is_found = false;
+                        // Checking if the corresponding HTML element can be find in the DOM
+                        // TODO: find the element associated to the category "Protractor test category".
+                        
+                        //TODO: how to elegantly solve the wait issue
+                        let all_span_elements_Promise = browser.driver.findElements(by.tagName('span'));  
+                        // code experiment from information gathered
+                        // browser.wait.until(ExpectedConditions.visibilityOf(by.xpath("//div[contains(text(),'Protractor test category')]"))); 
+                          
+                       
+                        
+                        //TODO: to remove when code finished
+                        // from https://www.selenium.dev/documentation/en/guidelines_and_recommendations/page_object_models/
+                        // verify h1 tag is "Hello userName" after login
+                        //browser.driver.findElement(by.tagName("h1")).isDisplayed();
+                        //let the_element = browser.driver.findElement(by.xpath("//li[contains(text()]",'Protractor test category'));
 
-                        all_li_elements_Promise.then(
-                            data=>{all_li_elements = data;},
-                            error=>{console.log("Error while retrieving all li elements:",error);}
-                        );
-                        all_li_elements.forEach(
-                            element => {
-                                
-                        });
 
+                        let all_span_elements:WebElement[];
+                        let protractor_test_category_is_found = false;                                        
+                        all_span_elements_Promise.then(
+                            data=>
+                            {
+                                all_span_elements = data;
+                                console.log("** 1. Retrieved all span elements. Number of elements found: "+all_span_elements.length);             
+                                all_span_elements.forEach(
+                                    element => 
+                                    {
+                                        let id:string;
+                                        //TODO: clean up code when information identification is successful 
+                                        let text:string
+                                        let cssValue_id:string;
+                                        let cssValue_value:string;
 
+                                        element.getText().then(
+                                            data => {
+                                                text=data;
+                                                console.log("**  1. Found text:"+text);
+                                            },
+                                            error => {
+                                                console.log("**  1. error while getting the element's text", error);
+                                            }                            
+                                        );
+                                        
+                                        /*
+                                        element.getId().then(
+                                            data => {
+                                                id=data; 
+                                                console.log("** 2. Found id:"+id); 
+                                                if(id.includes("Protractor test category"))
+                                                    {protractor_test_category_is_found = true;}  
+                                            },
+                                            error => {console.info("** 2. bis Error while getting the id:",error);}
+                                        );*/                                       
+                                        
+                                        /*
+                                        element.getCssValue("id").then(
+                                            data => {
+                                                cssValue_id=data;
+                                                console.log("** Extra: Found CssValue('id'):"+cssValue_id);
+                                            },
+                                            error => {
+                                                console.log("** Extra bis: error while getting the element's cssValue_id", error);
+                                            }                            
+                                        );
 
-                        browser.driver.sleep(5000);
+                                        element.getCssValue("value").then(
+                                            data => {
+                                                cssValue_value=data;
+                                                console.log("** Extra: Found CssValue('value'):"+cssValue_value);
+                                            },
+                                            error => {
+                                                console.log("** Extra bis: error while getting the element's cssValue_value", error);
+                                            }                            
+                                        );*/
+                                    });
+                             },                                      
+                             
+                            error=>
+                                {console.log("** 1. bis Error while retrieving all span elements:",error);}
+                        );            
+                      
+                        browser.driver.sleep(25000);
+                        //expect(protractor_test_category_is_found).toBe(true);
                         
 ;
                     } 
