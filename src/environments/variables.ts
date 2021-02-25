fff650f8805ab8da3c16912505bab87d98c2c682
Jean-Louis <jeanlouis.macle@gmail.com>
2021-02-25 11:39:50 +0100

    Corrected an issue with a local IP

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -3,8 +3,8 @@ export class Variables
     // Application that needed a rule in the firewall for the application to be accessed from 
     // another computer on the local network (with IP 192.168.1.101): 
     // C:\Program Files\nodejs\node.exe
-    //static base_url="http://localhost:8080"; //CORS issue for omitting http://
-    static base_url="http://192.168.1.100:8080"; //CORS issue for omitting http://
+    static base_url="http://localhost:8080"; //CORS issue for omitting http://
+    //static base_url="http://192.168.1.100:8080"; //CORS issue for omitting http://
 
 
 }
