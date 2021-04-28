a7a6ac18ee02cf7ab2f8c151943fca568c54abad
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2021-04-28 16:30:03 +0200

    Update variables.ts
    Restored a comment useful for a configuration check.

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -1,6 +1,9 @@
 export class Variables 
 {
    static base_url="http://localhost:8080"; //CORS issue for omitting http://    
+  
+  //Grid 4
+  //static base_url="http://<static_IP>:8080"; //CORS issue for omitting http://
 
 }
 
