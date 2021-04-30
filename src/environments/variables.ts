fbd102eac143a13c1df93590009381ef61f4a6cf
jlmacle <jeanlouis.macle@gmail.com>
2021-04-30 15:52:05 +0200

    Re-added a comment in variables.ts useful for a configuration check.

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -1,6 +1,8 @@
 export class Variables 
 {
    static base_url="http://localhost:8080"; //CORS issue for omitting http://    
-
+	
+	//useful for a configuration check
+	//static base_url="http://<static_IP>:8080"
 }
 
