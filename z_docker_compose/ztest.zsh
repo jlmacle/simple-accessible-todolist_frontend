32d706f602292ef5c9de88e9bfd334425ac2ffe7
Jean-Louis <jeanlouis.macle@gmail.com>
2021-05-07 10:56:50 +0200

    sonar.properties modification

--- z_docker_compose/ztest.zsh
+++ z_docker_compose/ztest.zsh
@@ -1,22 +1,22 @@
-#sudo docker prune system
+sudo docker system prune -f
 #fixed an issue with database initialisation
-
 echo "Stopping the postgreSQL Ubuntu service if running".
-sudo service postgresql stop
-echo "Stopping the nginx Ubuntu service if running".
-sudo service nginx stop
+sudo -u postgres /Library/PostgreSQL/13/bin/pg_ctl --pgdata=/Library/PostgreSQL/13/data stop &> /dev/null
 
 echo "Suppressing the services if they exist already"
 sudo docker service rm atl-front-end &> /dev/null
 sudo docker service rm atl-back-end &> /dev/null
 sudo docker service rm atl-postgres  &> /dev/null
+sleep 10
+sudo docker stack rm stack &> /dev/null
 
 echo "Building atl-network if necessary"
 sudo docker network create --driver overlay atl-network &> /dev/null
+sleep 5
 
-sudo docker stack deploy -c docker-compose-stack.yml stack
+#sudo docker stack deploy -c docker-compose-stack.yml stack
 
 #sudo docker stack deploy -c docker-compose-Azure.yml stack
-#sudo docker-compose up
+sudo docker-compose up
 
 chromium-browser http://127.0.0.1 &> /dev/null
