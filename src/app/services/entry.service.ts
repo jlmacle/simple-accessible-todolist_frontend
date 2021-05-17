f51c6d6415d5bcd8113738af00948e110fa30f2a
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2021-05-17 08:57:34 -0500

    Code linting.

--- src/app/services/entry.service.ts
+++ src/app/services/entry.service.ts
@@ -13,37 +13,37 @@ export class EntryService
 
   constructor(private http:HttpClient) { }
 
-  addCategory(category:Category):Promise<any>{
+  addCategory(category:Category): Promise<any>{
     const url = Variables.baseUrl+'/category';
     console.log('HTTP POST to: '+url);
     return this.http.post(url,category).toPromise();    
   }
 
-  getCategories():Promise<any>{
+  getCategories(): Promise<any>{
     return this.http.get(Variables.baseUrl+'/categories').toPromise();
   }
     
-  deleteCategory(id:number):Promise<any>{
+  deleteCategory(id:number): Promise<any>{
     const url = Variables.baseUrl+'/category/'+id;
     console.log('HTTP DELETE to: '+url);
     return this.http.delete(url).toPromise();    
   }
 
   
-  addItem(item:Item,categoryId:number):Promise<any>{
+  addItem(item:Item,categoryId:number): Promise<any>{
     const url = Variables.baseUrl+'/item/'+categoryId;
     console.log('HTTP POST to: '+url);
     console.log('Item data: Item id:'+item.id+', item name: '+item.name+', item category id'+categoryId);
     return this.http.post(url,item).toPromise();
   }
 
-  getItems():Promise<any>{
+  getItems(): Promise<any>{
     const url = Variables.baseUrl+'/items';
     console.log('HTTP GET to: '+url);
     return this.http.get(url).toPromise();
   }
 
-  deleteItem(id:number):Promise<any>{
+  deleteItem(id:number): Promise<any>{
     const url = Variables.baseUrl+'/item/'+id;
     console.log('HTTP DELETE to: '+url);
     return this.http.delete(url).toPromise();    
