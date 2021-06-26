bac9bd660a4d1f951fd40125f1f5b9704c8a1939
Jean-Louis Macle <59054324+jlmacle@users.noreply.github.com>
2021-06-26 16:35:36 -0500

    Fixed a backend configuration issue.

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -1,4 +1,4 @@
 export class Variables {
 	//static baseUrl = 'http://127.0.0.1:8080'; // CORS issue for omitting http://
-	static baseUrl='test-atl.azurewebsites.net'; // Useful for configuration check.
+	static baseUrl='test-atl.azurewebsites.net:8080'; // Useful for configuration check.
 }
