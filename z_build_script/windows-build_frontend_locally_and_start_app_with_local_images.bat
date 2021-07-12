3da3bad91bcb8ec80d8cc5cc794e776fa15c95f5
unknown <jeanlouis.macle@gmail.com>
2021-07-12 04:13:08 -0500

    Minor code cleanup.

--- z_build_script/windows-build_frontend_locally_and_start_app_with_local_images.bat
+++ z_build_script/windows-build_frontend_locally_and_start_app_with_local_images.bat
@@ -7,13 +7,13 @@ cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 :: Preparing the build of the Docker image
 ::--------------------------------------------------------------------------------------------------------------------
 cd ..
-echo "ng build"
+echo "ng build."
 start ng build --prod &
 
 echo "Waiting for the ng build to be finished."
 timeout /T 60
 
-echo "Website files moving in context folder"
+echo "Website files moving in context folder."
 xcopy  .\dist\AccessibleTodoList-FrontEnd\ .\z_build_script\context\html\ /s /e /y
 
 ::-----------------------------------------------------------------------------------------------------------------------------
@@ -31,16 +31,16 @@ echo "Testing docker stack with the new image."
 docker system prune -f
 ::fixed an issue with database initialisation
 
-::echo "Stopping the postgreSQL Ubuntu service if running".
+::echo "Stopping the postgreSQL Ubuntu service if running."
 ::No Postgresql service on current installation
 
-echo "Suppressing the services if they exist already"
+echo "Suppressing the services if they exist already."
 docker service rm atl-front-end 2> NUL
 docker service rm atl-back-end 2> NUL
 docker service rm atl-postgres  2> NUL
 ::https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-xp/bb490982(v=technet.10)
 
-echo "Building atl-network"
+echo "Building atl-network."
 docker network create --driver overlay atl-network 2> NUL
 
 docker stack deploy -c ../../z_docker_compose/docker-compose-stack_local-images.yml stack
