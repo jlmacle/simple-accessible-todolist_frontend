c308ade67087ca4b55b54a1bdff7a14592547302
jlmacle <jeanlouis.macle@gmail.com>
2021-07-21 15:47:34 +0200

    Code cleanup.

--- z_build_script/linux-check_code_quality-build_image.sh
+++ z_build_script/linux-check_code_quality-build_image.sh
@@ -1,49 +1,53 @@
 #-------------------------------------------------------------------------------------------------------------------
 # Checking potential npm issues
 #-------------------------------------------------------------------------------------------------------------------
-echo "An opportunity to check potential npm issues."
+echo "* An opportunity to check potential npm issues."
+echo "**** npm doctor"
 npm doctor
 #-------------------------------------------------------------------------------------------------------------------
 # Checking the potential npm security issues
 #-------------------------------------------------------------------------------------------------------------------
+echo "**** npm audit & npm audit fix."
 npm audit
 npm audit fix
 
 #-------------------------------------------------------------------------------------------------------------------
 # Checking the code quality with SonarQube
 #-------------------------------------------------------------------------------------------------------------------
-echo "Starting the SonarQube server."
+echo "* Checking the code quality with SonarQube."
+echo "**** Starting the SonarQube server."
 gnome-terminal -- bash -c 'sonar.sh start; sleep 90'
-echo "Waiting for the SonarQube server to start."
+echo "**** Waiting for the SonarQube server to start."
 sleep 90
 
-echo "Starting the analysis."
+echo "**** Starting the analysis."
 gnome-terminal -- bash -c 'cd .. ; sonar-scanner \
   -Dsonar.projectKey=front-end \
   -Dsonar.sources=. \
   -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.login=6fb38c8274cb8efccba8778aef56d226e7550659; sleep 100'
   
-echo "Waiting for the analysis to be done."
+echo "**** Waiting for the analysis to be done."
 sleep 40 
 
-echo "Starting a browser to check the result of the analysis."
+echo "**** Starting a browser to check the results."
 chromium-browser http://localhost:9000 &
 sleep 100
 
 #--------------------------------------------------------------------------------------------------------------------
 # Preparing the build of the Docker image
 #--------------------------------------------------------------------------------------------------------------------
+echo "* Docker image build process."
 cd ..
-echo "Starting the ng build."
+echo "**** Starting ng build --prod ."
 ng build --prod
 
-echo "Website files moving in context folder."
+echo "**** Website files moving in context folder."
 cp -Rfu dist/AccessibleTodoList-FrontEnd/*.* z_build_script/context/html
 cp -Rfu dist/AccessibleTodoList-FrontEnd/assets z_build_script/context/html
 
 
-echo "Building the new image."
+echo "**** Building the new image."
 cd z_build_script/context
 sudo docker build -t atl-front-end:v0.9 .
 cd ../..
