6fc26b1a62ab7ee664365915fa08a6a4ebdade1e
unknown <jeanlouis.macle@gmail.com>
2021-07-26 11:11:28 -0500

    style: output readability.

--- z_build_script/windows-no_code_quality_test_version-build_frontend_locally_and_start_app_with_local_images.bat
+++ z_build_script/windows-no_code_quality_test_version-build_frontend_locally_and_start_app_with_local_images.bat
@@ -1,34 +1,36 @@
 @echo off
-cls
-echo "* Starting Docker Desktop."
+::cls
+
+echo ** Starting Docker Desktop. **
 cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 :: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/cmd
 
+echo --------------------------------------------------------------------------------------------------------------------------------------------
+echo ** Docker build process. **
 ::--------------------------------------------------------------------------------------------------------------------
 :: Preparing the build of the Docker image
 ::--------------------------------------------------------------------------------------------------------------------
-echo "* Preparing the Docker image build."
-echo "**** Removing the files from the previous build (minus the pictures)."
+echo **** Removing the files from the previous build (minus the pictures).
 del /q .\context\html\
 
 cd ..
-echo "**** ng build."
-cmd /c ng build --configuration production &
+echo **** ng build.
+cmd /c "ng build --configuration production &"
 
-echo "**** Website files moving in context folder."
+echo **** Website files moving in context folder.
 xcopy  .\dist\AccessibleTodoList-FrontEnd\ .\z_build_script\context\html\ /s /e /y
 
 ::-----------------------------------------------------------------------------------------------------------------------------
 ::  Building the new image
 ::-----------------------------------------------------------------------------------------------------------------------------
-echo "* Building the Docker image."
+echo ***** Building the Docker image.
 cd .\z_build_script\context\
 docker build -t atl-front-end:v0.9 .
-
+echo --------------------------------------------------------------------------------------------------------------------------------------------
 ::------------------------------------------------------------------------------------------------------------------------------
 :: Testing docker stack with the new image updated on DockerHub
 ::------------------------------------------------------------------------------------------------------------------------------
-echo "* Testing Docker stack with the new image."
+echo ** Testing Docker stack with the new image. **
 
 docker system prune -f
 ::fixed an issue with database initialisation
@@ -36,7 +38,7 @@ docker system prune -f
 ::echo "Stopping the postgreSQL Ubuntu service if running."
 ::No Postgresql service on current installation
 
-echo "Suppressing the services if they exist already."
+echo ** Suppressing the services if they exist already."
 docker service rm atl-front-end 2> NUL
 docker service rm atl-back-end 2> NUL
 docker service rm atl-postgres  2> NUL
