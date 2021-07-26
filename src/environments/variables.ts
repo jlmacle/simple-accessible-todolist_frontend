15121637c0d99a0785eaf7b57baf03276b9898e6
unknown <jeanlouis.macle@gmail.com>
2021-07-26 09:24:44 -0500

    Fix: Re-added information useful for configuration check.

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -2,4 +2,5 @@ export class Variables {
 
 	static baseUrl = 'http://127.0.0.1:8080'; // CORS issue for omitting http://
 	//static baseUrl='https://test-atl.azurewebsites.net:8080'; // Useful for configuration check.
+	//static baseUrl='static base_url="http://<static_IP>:8080";' // Useful for configuration check.
 }
