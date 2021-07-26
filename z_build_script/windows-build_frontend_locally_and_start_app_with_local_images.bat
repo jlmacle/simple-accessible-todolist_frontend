0dd4d6f4a58e4b28d676e872703e4c23a06bfd6f
unknown <jeanlouis.macle@gmail.com>
2021-07-26 09:38:44 -0500

    style: script used to build the Docker image locally and starting the app to evaluate the result.

--- z_build_script/windows-build_frontend_locally_and_start_app_with_local_images.bat
+++ z_build_script/windows-build_frontend_locally_and_start_app_with_local_images.bat
@@ -1,32 +1,34 @@
-::--------------------------------------------------------------------------------------------------------------------
-:: Starting Docker Desktop
-::-------------------------------------------------------------------------------------------------------------
+@echo off
+cls
+echo "* Starting Docker Desktop."
 cmd /c "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+:: https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/cmd
 
 ::--------------------------------------------------------------------------------------------------------------------
 :: Preparing the build of the Docker image
 ::--------------------------------------------------------------------------------------------------------------------
+echo "* Preparing the Docker image build."
+echo "**** Removing the files from the previous build (minus the pictures)."
+del /q .\context\html\
+
 cd ..
-echo "ng build."
-start ng build --prod &
+echo "**** ng build."
+cmd /c ng build --configuration production &
 
-echo "Waiting for the ng build to be finished."
-timeout /T 60
-
-echo "Website files moving in context folder."
+echo "**** Website files moving in context folder."
 xcopy  .\dist\AccessibleTodoList-FrontEnd\ .\z_build_script\context\html\ /s /e /y
 
 ::-----------------------------------------------------------------------------------------------------------------------------
 ::  Building the new image
 ::-----------------------------------------------------------------------------------------------------------------------------
-
+echo "* Building the Docker image."
 cd .\z_build_script\context\
 docker build -t atl-front-end:v0.9 .
 
 ::------------------------------------------------------------------------------------------------------------------------------
 :: Testing docker stack with the new image updated on DockerHub
 ::------------------------------------------------------------------------------------------------------------------------------
-echo "Testing docker stack with the new image."
+echo "* Testing Docker stack with the new image."
 
 docker system prune -f
 ::fixed an issue with database initialisation
@@ -45,8 +47,12 @@ docker network create --driver overlay atl-network 2> NUL
 
 docker stack deploy -c ../../z_docker_compose/docker-compose-stack_local-images.yml stack
 echo "Note: the front-end might take a little while to start."
+cd ..
 timeout /T 200
 start msedge http://127.0.0.1 
-cd ..
 
 
+
+:: Troubleshouting
+:: docker container exec   <container_id>  ls -lah usr/share/nginx/html
+:: docker container exec   77321f45353a   ls -lah usr/share/nginx/html
