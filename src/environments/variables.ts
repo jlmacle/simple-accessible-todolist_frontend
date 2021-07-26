c82172c50cba372456b6dedb75254ff0fbbdb30d
unknown <jeanlouis.macle@gmail.com>
2021-07-26 10:19:19 -0500

    style: addition of spaces.

--- src/environments/variables.ts
+++ src/environments/variables.ts
@@ -1,7 +1,8 @@
 export class Variables {
 
 	static baseUrl = 'http://127.0.0.1:8080'; // CORS issue for omitting http://
-	//static baseUrl='https://test-atl.azurewebsites.net:8080'; // Useful for configuration check.
-	// static baseUrl='http://<static_IP>:8080' // Useful for configuration check.
+	//static baseUrl = 'https://test-atl.azurewebsites.net:8080'; // Useful for configuration check.
+	// static baseUrl = 'http://<static_IP>:8080'; // Useful for configuration check.
+	
 	
 }
